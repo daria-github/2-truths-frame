05ce56fdbfd87ef963d95e6ada3fdf6e5a3f9b39
daria-github <daria@xmtp.com>
2024-06-18 16:13:33 -0700

    updated order and use id instead of query params

--- src/app/api/og/secondFrame/route.tsx
+++ src/app/api/og/secondFrame/route.tsx
@@ -1,38 +1,73 @@
 import { ImageResponse } from "next/og";
 import fs from "fs";
 import path from "path";
-// import { shuffleArray } from "../firstFrame/route";
 
 // DARICK: This is the second frame image with the results
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
-    const data = searchParams.get("data") || "";
-    const parsedData = JSON.parse(decodeURI(data));
+    const id = searchParams.get("id") || "";
 
-    const { truth1, truth2, lie, name } = parsedData;
+    const displayFilePath = path.join(
+      process.cwd(),
+      "data",
+      id,
+      "displayOrder.txt"
+    );
+    const displayFileItems = fs.readFileSync(displayFilePath, "utf8");
+    const parsedItems = JSON.parse(displayFileItems);
 
-    // Read votes stored in node filesystem
-    // To-do: change this to go by randomized ID instead of name
-    const dataDirectory = path.join(process.cwd(), "data");
-    const filePath = path.join(dataDirectory, `votes-${name}.json`);
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    const listOfVotes = Array.from(fileContents);
+    const truth1FilePath = path.join(process.cwd(), "data", id, "truth1.txt");
+    const truth2FilePath = path.join(process.cwd(), "data", id, "truth2.txt");
+    const nameFilePath = path.join(process.cwd(), "data", id, "name.txt");
+    const lieFilePath = path.join(process.cwd(), "data", id, "lie.txt");
+    const votesFilePath = path.join(process.cwd(), "data", id, "votes.txt");
 
-    const truth1Count = listOfVotes.filter((vote) => vote === "1").length;
-    const truth2Count = listOfVotes.filter((vote) => vote === "2").length;
-    const lieCount = listOfVotes.filter((vote) => vote === "3").length;
+    const truth1 = fs.readFileSync(truth1FilePath, "utf8");
+    const truth2 = fs.readFileSync(truth2FilePath, "utf8");
+    const name = fs.readFileSync(nameFilePath, "utf8");
+    const lie = fs.readFileSync(lieFilePath, "utf8");
+    const votes = fs.readFileSync(votesFilePath, "utf8").split("\n");
+
+    const truth1Count = votes.filter((vote) => vote === "truth1").length;
+    const truth2Count = votes.filter((vote) => vote === "truth2").length;
+    const lieCount = votes.filter((vote) => vote === "lie").length;
+
+    const firstItemVotes =
+      parsedItems[0] === "truth1"
+        ? `${truth1}: ${truth1Count}`
+        : parsedItems[0] === "truth2"
+        ? `${truth2}: ${truth2Count}`
+        : `${lie}: ${lieCount}`;
+
+    const secondItemVotes =
+      parsedItems[1] === "truth1"
+        ? `${truth1}: ${truth1Count}`
+        : parsedItems[1] === "truth2"
+        ? `${truth2}: ${truth2Count}`
+        : `${lie}: ${lieCount}`;
+
+    const thirdItemVotes =
+      parsedItems[2] === "truth1"
+        ? `${truth1}: ${truth1Count}`
+        : parsedItems[2] === "truth2"
+        ? `${truth2}: ${truth2Count}`
+        : `${lie}: ${lieCount}`;
+
+    const allResults = [firstItemVotes, secondItemVotes, thirdItemVotes];
 
     return new ImageResponse(
       (
         <div tw="flex flex-col w-full py-12 px-4 md:items-left justify-between p-8 bg-black min-h-screen">
-          <h1 tw="mt-4 mb-6 text-center text-4xl font-bold leading-9 text-white">{`${name} has told Two Truths and a Lie!`}</h1>
+          <h1 tw="mt-4 mb-6 text-center text-4xl font-bold leading-9 text-white">
+            {`${name} has told Two Truths and a Lie!`}
+          </h1>
           <h2 tw="flex flex-col text-3xl sm:text-3xl font-bold tracking-tight text-white text-left">
             Poll results
           </h2>
-          <h3 tw="text-2xl font-semibold text-white">{`${truth1}: ${truth1Count}`} votes</h3>
-          <h3 tw="text-2xl font-semibold text-white">{`${truth2}: ${truth2Count}`} votes</h3>
-          <h3 tw="text-2xl font-semibold text-white">{`${lie}: ${lieCount}`} votes</h3>
+          <h3 tw="text-2xl font-semibold text-white">{`${firstItemVotes} votes`}</h3>
+          <h3 tw="text-2xl font-semibold text-white">{`${secondItemVotes} votes`}</h3>
+          <h3 tw="text-2xl font-semibold text-white">{`${thirdItemVotes} votes`}</h3>
         </div>
       )
     );
--- src/app/secondFrame/page.js
+++ src/app/secondFrame/page.js
@@ -10,17 +10,26 @@ export let metadata = {
 const SecondFrame = (props) => {
   const vote = Number(props.searchParams.vote);
   const id = props.searchParams.id;
-  const parsedData = JSON.parse(decodeURI(data));
 
-  const dataDirectory = path.join(process.cwd(), "data", id);
   const votesFilePath = path.join(process.cwd(), "data", id, "votes.txt");
+  const orderFilePath = path.join(
+    process.cwd(),
+    "data",
+    id,
+    "displayOrder.txt"
+  );
+  const displayOrder = fs.readFileSync(orderFilePath, "utf-8");
 
-  // Ensure the data directory exists
-  if (!fs.existsSync(dataDirectory)) {
-    fs.mkdirSync(dataDirectory);
+  if (vote === 1) {
+    const votedFor = JSON.parse(displayOrder)[0];
+    fs.appendFileSync(votesFilePath, `${votedFor}\n`);
+  } else if (vote === 2) {
+    const votedFor = JSON.parse(displayOrder)[1];
+    fs.appendFileSync(votesFilePath, `${votedFor}\n`);
+  } else if (vote === 3) {
+    const votedFor = JSON.parse(displayOrder)[2];
+    fs.appendFileSync(votesFilePath, `${votedFor}\n`);
   }
-
-  fs.appendFileSync(filePath, `${vote}`);
 
   const frameMetadata = getFrameMetadata({
     accepts: { xmtp: "2024-02-09" },
@@ -29,10 +38,10 @@ const SecondFrame = (props) => {
       {
         label: "See Correct Answer",
         action: "post",
-        target: `${process.env.NEXT_PUBLIC_BASE_URL}/thirdFrame?data=${data}`,
+        target: `${process.env.NEXT_PUBLIC_BASE_URL}/thirdFrame?id=${id}`,
       },
     ],
-    image: `${process.env.NEXT_PUBLIC_BASE_URL}/api/og/secondFrame?data=${data}`,
+    image: `${process.env.NEXT_PUBLIC_BASE_URL}/api/og/secondFrame?id=${id}`,
   });
 
   metadata = {
