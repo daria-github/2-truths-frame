ac788b727d84541add6f00c0dfa93377ff323cd9
Darick Dang <darickdang@gmail.com>
2024-06-18 10:51:33 -0700

    2 page CSS update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,10 +25,10 @@ function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
 // DARICK — This is the entry point/template!
 export default async function Home() {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-10 w-auto" src="https://xmtp.org/img/builtWithXmtp/xmtp.svg" alt="Your Company" />
-        <h2 className="mt-4 text-center text-2xl font-bold leading-9 text-white">2 Truths and a Lie</h2>
+    <div className="flex min-h-full flex-col justify-center px-6 py-2 lg:px-8 h-screen">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm m-auto">
+        <img className="mx-auto h-10 w-auto" src="https://xmtp.org/img/builtWithXmtp/xmtp.svg" alt="XMTP Logo" />
+        <h2 className="mt-4 text-center text-3xl font-bold leading-9 text-white">2 Truths and a Lie</h2>
         <p className="mt-2 text-center text-base font-normal leading-6 text-gray-400">Enter your 2 truth and a lie statements and a frame will be created for others to guess.</p>
         <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
           <PollCreateForm />
