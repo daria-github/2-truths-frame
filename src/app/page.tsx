ca4651c2023acb9a6cd9e3b2dde8592a3a94ff35
Darick Dang <darickdang@gmail.com>
2024-06-20 21:28:15 -0700

    New graphic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-2 lg:px-8 h-screen">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm m-auto">
-        <img className="mx-auto h-80 w-auto" src="https://github.com/daria-github/2-truths-frame/blob/46b2b6f01033528cdf55b3000444756b762d5139/public/eyeGraphic.png?raw=true" alt="XMTP Logo" />
-        <h2 className="-mt-14 text-center text-4xl font-semibold leading-9 text-white font-playfair drop-shadow-lg"><span className="text-5xl italic font-bold">2</span> Truths and a Lie</h2>
+        <img className="mx-auto h-80 w-auto" src="https://raw.githubusercontent.com/daria-github/2-truths-frame/0ca270b2f814936e8d077dd784410857611a6a2c/public/2Truths.png" alt="XMTP Logo" />
         <p className="mt-1 text-center text-lg font-normal text-gray-400 leading-6 font-playfair">Enter your 2 truths and 1 lie statements and a frame will be created for others to guess.</p>
         <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
           <PollCreateForm />
